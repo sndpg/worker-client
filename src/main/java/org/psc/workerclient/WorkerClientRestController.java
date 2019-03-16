009ae7a419f0c38864425d82fa1c550748d24cba
sykq <genericmailacc@gmail.com>
2019-03-16 15:05:49 +0100

    misc

--- src/main/java/org/psc/workerclient/WorkerClientRestController.java
+++ src/main/java/org/psc/workerclient/WorkerClientRestController.java
@@ -1,17 +1,16 @@
 package org.psc.workerclient;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.AbstractMap;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Map;
+import java.util.TreeMap;
 
 @RestController("/")
 public class WorkerClientRestController {
@@ -20,7 +19,7 @@ public class WorkerClientRestController {
     public List<Double> getRandomNumber() {
         WebClient client = WebClient.builder().baseUrl("http://localhost:8080").build();
 
-        Flux<Double> response = client.get()
+        Flux<Double> randomNumberResponse = client.get()
                 .uri("/functional/flux/randomNumber")
                 .retrieve()
                 .bodyToFlux(Double.class)
@@ -28,7 +27,42 @@ public class WorkerClientRestController {
                 .limitRequest(50)
                 .doOnEach(System.out::println);
 
-        response.publish().materialize();
-        return response.collectList().block();
+        randomNumberResponse.publish().materialize();
+        return randomNumberResponse.collectList().block();
     }
+
+    @GetMapping("/mix")
+    public Map<LocalDateTime, String> getMix() {
+        WebClient client = WebClient.builder().baseUrl("http://localhost:8080").build();
+
+        var resultMap = new TreeMap<LocalDateTime, String>();
+        Flux<AbstractMap.SimpleEntry<LocalDateTime, String>> randomNumberResponse = client.get()
+                .uri("/functional/flux/randomNumber")
+                .retrieve()
+                .bodyToFlux(Double.class)
+                .filter(d -> d >= 0.5d)
+                .map(e -> new AbstractMap.SimpleEntry<>(LocalDateTime.now(), String.valueOf(e)))
+                .limitRequest(12)
+                .doOnEach(System.out::println)
+//                .doOnEach(e -> resultMap.put(e.get().getKey(), e.get().getValue()))
+                .onErrorStop();
+
+        Flux<AbstractMap.SimpleEntry<LocalDateTime, String>> timeResponse = client.get()
+                .uri("functional/flux/time")
+                .retrieve()
+                .bodyToFlux(LocalDateTime.class)
+                .map(e -> new AbstractMap.SimpleEntry<>(LocalDateTime.now(), e.format(DateTimeFormatter.ofPattern("yyyyMMdd-hh:mm:ss.nnnnnnnnn"))))
+                .limitRequest(7)
+                .doOnEach(System.out::println)
+//                .doOnEach(e -> resultMap.put(e.get().getKey(), e.get().getValue()))
+                .onErrorStop();
+
+        randomNumberResponse.subscribe(e -> resultMap.put(e.getKey(), e.getValue()));
+        timeResponse.subscribe(e -> resultMap.put(e.getKey(), e.getValue()));
+
+        Flux.merge(randomNumberResponse, timeResponse).blockLast();
+
+        return resultMap;
+    }
+
 }
