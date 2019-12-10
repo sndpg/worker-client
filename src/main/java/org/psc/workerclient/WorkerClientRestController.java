dce35dde7a9ac8542df867df91786858246636d7
sykq <genericmailacc@gmail.com>
2019-12-10 15:14:38 +0100

    misc

--- src/main/java/org/psc/workerclient/WorkerClientRestController.java
+++ src/main/java/org/psc/workerclient/WorkerClientRestController.java
@@ -1,10 +1,25 @@
 package org.psc.workerclient;
 
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.frame.decoder.PayloadDecoder;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import lombok.RequiredArgsConstructor;
+import org.reactivestreams.Publisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.MetadataExtractor;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.netty.tcp.TcpClient;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.AbstractMap;
@@ -12,8 +27,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+@RequiredArgsConstructor
 @RestController("/")
 public class WorkerClientRestController {
+
+    private final RSocketRequester rSocketRequester;
 
     @GetMapping
     public List<Double> getRandomNumber() {
@@ -51,7 +69,8 @@ public class WorkerClientRestController {
                 .uri("functional/flux/time")
                 .retrieve()
                 .bodyToFlux(LocalDateTime.class)
-                .map(e -> new AbstractMap.SimpleEntry<>(LocalDateTime.now(), e.format(DateTimeFormatter.ofPattern("yyyyMMdd-hh:mm:ss.nnnnnnnnn"))))
+                .map(e -> new AbstractMap.SimpleEntry<>(LocalDateTime.now(),
+                        e.format(DateTimeFormatter.ofPattern("yyyyMMdd-hh:mm:ss.nnnnnnnnn"))))
                 .limitRequest(7)
                 .doOnEach(System.out::println)
 //                .doOnEach(e -> resultMap.put(e.get().getKey(), e.get().getValue()))
@@ -63,6 +82,32 @@ public class WorkerClientRestController {
         Flux.merge(randomNumberResponse, timeResponse).blockLast();
 
         return resultMap;
+    }
+
+    @GetMapping(value = "/randomDecimals", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Publisher<BigDecimal> getRandomBigDecimals() {
+        return rSocketRequester.route("randomDecimals")
+                .retrieveFlux(BigDecimal.class);
+    }
+
+    @Configuration
+    public static class RSocketConfiguration {
+
+        @Bean
+        public RSocket rSocket() {
+            return RSocketFactory.connect()
+                    .mimeType("message/x.rsocket.routing.v0", MediaType.APPLICATION_JSON_VALUE)
+                    .frameDecoder(PayloadDecoder.ZERO_COPY)
+                    .transport(TcpClientTransport.create(TcpClient.create().port(7777).host("localhost")))
+                    .start()
+                    .block();
+        }
+
+        @Bean
+        public RSocketRequester rSocketRequester(RSocketStrategies rSocketStrategies) {
+            return RSocketRequester.wrap(rSocket(), MimeTypeUtils.APPLICATION_JSON,
+                    MimeTypeUtils.parseMimeType("message/x.rsocket.routing.v0"), rSocketStrategies);
+        }
     }
 
 }
